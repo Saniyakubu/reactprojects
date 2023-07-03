46bf63615a152362f5561149f39f749a3ff5afc3
sani <ysani095@gmail.com>
2023-07-04 02:11:56 +0800

    update

--- practicefile/src/App.js
+++ practicefile/src/App.js
@@ -1,24 +1,9 @@
 import './App.css';
-import { useState } from 'react';
-
+import Box1 from './box1';
 function App() {
-  const [checkboxx, setCheckboxx] = useState('mercedes');
-  console.log(checkboxx);
-
-  function handleChanges(e) {
-    setCheckboxx(e.target.value);
-  }
-
   return (
     <div className="App">
-      <form>
-        <select name="select" id="" value={checkboxx} onChange={handleChanges}>
-          <option value="mercedes">mercedes</option>
-          <option value="audi">audi</option>
-          <option value="tesla">tesla</option>
-          <option value="volvo">volvo</option>
-        </select>
-      </form>
+      <Box1 />
     </div>
   );
 }
